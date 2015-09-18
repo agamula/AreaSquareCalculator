26110914cab3d32ad8be5db7cec735b7689297e8
agamula <nativetiger90@gmail.com>
2015-09-18 16:02:30 +0300

    Add project on github

--- app/src/main/java/com/proggroup/areasquarecalculator/MainActivity.java
+++ app/src/main/java/com/proggroup/areasquarecalculator/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.main);
 
         mResultEdit = (EditText)findViewById(R.id.result);
