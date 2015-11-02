b457aa1e0d9f18caa459436ba904ec4e6892c93c
agamula <agamula90@gmail.com>
2015-11-02 23:08:04 +0200

    [CHANGE] data reverted

--- app/src/main/java/com/proggroup/squarecalculations/DocParser.java
+++ app/src/main/java/com/proggroup/squarecalculations/DocParser.java
@@ -63,14 +63,14 @@ public class DocParser {
     private static long parseTime(String time) {
         String[] splitValues = time.split(":");
 
-        int hours = Integer.parseInt(splitValues[0]);
+        //int hours = Integer.parseInt(splitValues[0]);
 
-        int minutes = Integer.parseInt(splitValues[1]);
+        int minutes = Integer.parseInt(splitValues[0]);
 
-        String secondMillisVal = splitValues[2];
+        String secondMillisVal = splitValues[1];
 
         int seconds = Integer.parseInt(secondMillisVal.substring(0, secondMillisVal.indexOf('.')));
 
-        return TimeUnit.HOURS.toSeconds(hours) + TimeUnit.MINUTES.toSeconds(minutes) + TimeUnit.SECONDS.toSeconds(seconds);
+        return /*TimeUnit.HOURS.toSeconds(hours) +*/ TimeUnit.MINUTES.toSeconds(minutes) + TimeUnit.SECONDS.toSeconds(seconds);
     }
 }
